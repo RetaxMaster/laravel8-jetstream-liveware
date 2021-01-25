f01a9a2a5eaafdd23472bae8af4fb34ace0b4217
RetaxMaster <retaxmaster@hotmail.com>
2021-01-25 16:35:30 -0600

    Adicion de las migraciones

--- database/migrations/2021_01_25_223043_create_posts_table.php
+++ database/migrations/2021_01_25_223043_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,17 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
 
+            $table->unsignedBigInteger("course_id");
+
             $table->string("name");
+            $table->boolean("free")->default(0);
+
+            $table->timestamps();
+
+            $table->foreign("course_id")->references("id")->on("courses");
 
             $table->timestamps();
         });
@@ -29,6 +36,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('posts');
     }
 }
