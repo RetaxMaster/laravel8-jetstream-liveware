1638f31c372fcc691a71a83ce8e3c7d739df0168
RetaxMaster <retaxmaster@hotmail.com>
2021-01-26 09:43:03 -0600

    Carga de cursos en el apartado de contenido

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -6,10 +6,23 @@
 
     <div class="p-8 bg-gray-200 col-span-1">
         <ul class="felx flex-col">
+
             <li class="font-medium text-sm text-gray-400 uppercase mb-4">
                 Contenido
             </li>
-            ---
+
+            @foreach ($course->posts as $post)
+                <li class="felx items-center text-gray-600 mt-2">
+
+                    {{ $post->name }}
+
+                    @if ($post->free)
+                        <span class="text-xs text-gray-500 font-semibold bg-gray-300 px-2 rounded-full ml-auto">Gratis</span>
+                    @endif
+                    
+                </li>
+            @endforeach
+
         </ul>
     </div>
 
