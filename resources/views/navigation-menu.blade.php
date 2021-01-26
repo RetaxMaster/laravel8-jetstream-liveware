b511050818280bf95ecaf3f42d50d2509d89c457
RetaxMaster <retaxmaster@hotmail.com>
2021-01-26 10:22:21 -0600

    Modificacion del dasboard

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,9 +12,15 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+
+                    <x-jet-nav-link href="{{ route('home') }}">
+                        {{ __('Sitio Web') }}
+                    </x-jet-nav-link>
+
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+
                 </div>
             </div>
 
@@ -138,9 +144,15 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+
+            <x-jet-responsive-nav-link href="{{ route('home') }}">
+                {{ __('Sitio web') }}
+            </x-jet-responsive-nav-link>
+
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
+
         </div>
 
         <!-- Responsive Settings Options -->
